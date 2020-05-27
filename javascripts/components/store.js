ff86b070b43d7dcccd594f84d6ac67b47e71e92b
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-05-26 19:17:04 -0500

    created store component

---
+++ javascripts/components/store.js
@@ -0,0 +1,25 @@
+import bookData from '../helpers/data/bookData.js';
+import utils from '../helpers/utils.js'
+
+const addToCartEvent = (e) => {
+  e.preventDefault();
+
+  console.log('You added book to cart');
+}
+
+const makeStore = () => {
+  const bookInfo = bookData.getBook();
+
+  const domString = `
+    <h2>Our One Book</h2>
+    Buy Now! It's only 
+    <h3 id="book-price">$${bookInfo.price}</h3>
+    <button class="btn btn-danger" id="add-to-cart">Add To Cart</button>
+  `;
+   
+  utils.printToDom('#store', domString);
+
+  document.querySelector("#add-to-cart").addEventListener("click", addToCartEvent);
+}
+
+export default { makeStore }
